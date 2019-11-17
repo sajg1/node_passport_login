03cf2a8e28ba522ee32ced9e8e74c5eab4732642
Steve Gossip <stephengossip@gmail.com>
2019-11-17 18:57:06 +0000

    added the auth.js config file in order to check for Authentication. If a valid login has happened it will allow you to procedd. This is then passed as a parameter in the dashboard .get method which stops a user accessing the dashboard by typing the route into the address bar.

---
+++ config/auth.js
@@ -0,0 +1,10 @@
+// Passport gives a method for req.isAuthenticated
+module.exports = {
+    ensureAuthenticated: function(req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+        req.flash('error_msg', 'Please log in to view the dashboard');
+        res.redirect('/users/login');
+    }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
 // Welcome page
 // changed from send to render in order to render the welcome.ejs view
 router.get('/', (req, res) => res.render('welcome'));
 
 // Dashboard Page
-router.get('/dashboard', (req, res) => res.render('dashboard'));
+router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('dashboard'));
 
 module.exports = router;
