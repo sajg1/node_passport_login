60b4c763b9c55b89ce05946392c740f5e0c6c64a
Steve Gossip <stephengossip@gmail.com>
2019-11-17 15:54:46 +0000

    required bcrypt and User.js into users.js. Then added another check in post. If validiation is passed I check if a user already exists. If it does the register form is re-render and adds an error.If not, a new User is created

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+// User
+const User = require('../models/User');
 
 //Login Page
 router.get('/login', (req, res) => res.render('login'));
@@ -35,7 +39,32 @@ router.post('/register', (req, res) => {
             password2
         })
     } else {
-        res.send('pass');
+        // Validation is passed
+
+        // returns a promise and can now access user details
+        User.findOne({ email: email})
+        .then (user => {
+            if(user) {
+                // User already exists
+                errors.push({ msg: 'Email is already registered'})
+                res.render('register', {
+                    errors,
+                    name,
+                    email,
+                    password,
+                    password2
+                });
+            } else {
+                const newUser = new User({
+                    name,
+                    email,
+                    password
+                });
+
+                console.log(newUser);
+                res.send('hello');
+            }
+        });
     }
 });
 
