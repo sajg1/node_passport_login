edb9eac71bdafedc39ff7382e90b9137a6bc1bc4
Steve Gossip <stephengossip@gmail.com>
2019-11-17 15:28:01 +0000

    req.body was deconstructed for easier access. Then created an errors array that is followed by a number of checks that will push an error to array if true. Finally, if any errors are pushed then we re-render the form and pass the values.

--- routes/users.js
+++ routes/users.js
@@ -9,8 +9,34 @@ router.get('/register', (req,res) => res.render('register'));
 
 // Register Handle (handles posting of form in register.ejs to /users/register)
 router.post('/register', (req, res) => {
-    console.log(req.body);
-    res.send('hello');
+    // used to pull elements out of req.body
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+    // Check all fields are required, otherwise send message
+    if (!name || !email || !password || !password2) {
+        errors.push({msg : 'Please fill in all fields'})
+    }
+    // Check passwords match
+    if (password !== password2) {
+        errors.push({msg: 'Passwords do not match'})
+    }
+    // Check password length
+    if (password.length < 6) {
+        errors.push({msg: 'Password should be at least 6 characters'})
+    }
+    // If any errors are pushed, I want to re-render the register form
+    // When rendering with a template it is possible to pass in values like below
+    if (errors.length > 0 ) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        })
+    } else {
+        res.send('pass');
+    }
 });
 
 module.exports = router;
